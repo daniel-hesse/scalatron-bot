8620a218845c52578711fe93b1429172684140d9
Matthias Jambach <mjambach@autoscout24.com>
2016-10-09 16:04:18 +0200

    Some minor fixes ;-)

--- src/main/scala/Bot.scala
+++ src/main/scala/Bot.scala
@@ -1,16 +1,11 @@
 // mjambach bot
-import scala.util.control.NonFatal
 import util.Random
 
 class ControlFunction {
-  var Count = 90
-  var X = 1
-  var Y = 0
   val rnd = new Random()
-  var dir = ""
-  var x = 0
-  var y = 0
-  var steps = 0
+  var maxSlaves = 300
+  var roundTime = 0
+  var currentDir = "0:0"
 
   def respond(input: String): String = try {
     tryrespond(input)
@@ -24,74 +19,148 @@ class ControlFunction {
     val (opcode, paramMap) = CommandParser(input) //parseResult = parse(input)
     var command = List[String]()
 
-
     if (opcode == "React") {
-      // token(0): 0th element of array
       val generation = paramMap("generation").toInt
-
+      val timeRemaining = roundTime - paramMap("time").toInt
+      val energy = paramMap("energy").toInt
+      val collision = paramMap.getOrElse("collision", "")
+      val slaveCount = paramMap("slaves").toInt
+      val viewString = paramMap.getOrElse("view", "")
+      val view = View(viewString)
+      var dir = ""
       //Master Node
       if (generation == 0) {
-        val energy = paramMap("energy").toInt
-        val collision = paramMap.getOrElse("collision", "")
-        val slaveCount = paramMap("slaves").toInt
-        val viewString = paramMap.getOrElse("view", "")
-        val view = View(viewString)
+        dir = currentDir
 
-        checkDirection(view)
+        // Debug output of view
+        if (dir != "") {
+          var viewdir = view.cellAtRelPos(XY(dir))
+          command = s"Status(text=$viewdir)" +: command
+        }
 
+        // Check current direction and make some decisions
+        dir = checkDirection(dir, view, generation, slaveCount, timeRemaining)
         command = s"Move(direction=$dir)" +: command
-        steps = steps + 1
 
-        if (energy >= 100 && slaveCount <= 30) {
+        if (energy >= 100 && slaveCount <= 10 && timeRemaining >= 100) {
           val botDir = XY(dir).negate
           command = s"Spawn(direction=$botDir,energy=100,heading=$botDir)" +: command
         }
-
+        currentDir = dir
       } else {
         //Slave
-        val master = paramMap("master")
-        val heading = paramMap("heading")
-        val viewString = paramMap.getOrElse("view", "")
-        val view = View(viewString)
+        var dir = paramMap("heading")
+        var masterDir = paramMap("master")
+        masterDir = XY(masterDir).signum.toString
 
-        checkDirection(view)
+        // Enhance this:
+        // - Should explode if enemy is two stepps away instead of one
+        // - Should be function like checkEnemies
+
+        if (slaveCount >= 20 && (view.cellAtRelPos(XY(dir)) == 'm' || view.cellAtRelPos(XY(dir)) == 's') ) {
+          command = s"Explode(size=4)" +: command
+        }
+
+        dir = checkDirection(dir, view, generation, slaveCount, timeRemaining)
+
+        if (slaveCount >= 10 && (view.cellAtRelPos(XY(dir)) == 'm' || view.cellAtRelPos(XY(dir)) == 's')) {
+          command = s"Explode(size=4)" +: command
+        }
+
+        // If energy is enough, slaveCount below 100 and enough time, spawn new bots
+        if (energy >= 100 && slaveCount <= 300 && timeRemaining >= 100) {
+          val botDir = XY(dir).negate
+          command = s"Spawn(direction=$botDir,energy=100,heading=$botDir)" +: command
+        }
+        if (timeRemaining <= 100) {
+          // Overwrite direction to masterDirection if time is near end
+          dir = masterDir
+        }
 
         command = s"Move(direction=$dir)" +: command
       }
 
+      // Send command to server
       command.mkString("|")
+    } else if (opcode == "Welcome") {
+      roundTime = paramMap("apocalypse").toInt
+      if (paramMap("maxslaves").toInt != 0) { maxSlaves = paramMap("maxslaves").toInt }
+      ""
     } else {
-      "" // response if false
+      ""
     }
   }
 
-  def checkDirection(view: View) = {
-    view.offsetToNearest('B').foreach (offset => dir = offset.signum.toString)
-    view.offsetToNearest('P').foreach (offset => dir = offset.signum.toString)
+  def checkDirection(curdir: String, view: View, generation: Int, slaveCount: Int, timeRemaining: Int): String = {
+    var dir = curdir
+    // Fix for empty dir
+    if (dir == "") { var dir = generateDirection(view) }
 
-    //Check direction
-    if (dir == "") { generateDirection(view) }
-    var viewdir = view.cellAtRelPos(XY(dir))
+    if (timeRemaining >= 100) {
+      // Check direction
+      // Master should just eat static plants
+      // Gen1 slaves can eat plants and good beasts but should not die
+      // Gen2+ slaves (with low energy) can eat plants, good plants and enemy masters/slaves
+      // Gen2+ slaves with high energy should prefer own Master to give energy back
+      if (generation == 0) { // Master
+        view.offsetToNearest('P').foreach(offset => dir = offset.signum.toString)
 
-    if (viewdir != '_' && viewdir != 'P' && viewdir != 'B') {
-      generateDirection(view)
+        var viewdir = view.cellAtRelPos(XY(dir))
+        if (viewdir != '_' && viewdir != 'P' && viewdir != 'B' && viewdir != 'S') { dir = generateDirection(view) }
+      } else if (generation >= 1 && slaveCount < 20) {
+        view.offsetToNearest('B').foreach (offset => dir = offset.signum.toString)
+        view.offsetToNearest('P').foreach (offset => dir = offset.signum.toString)
+
+        var viewdir = view.cellAtRelPos(XY(dir))
+        if (viewdir != '_' && viewdir != 'P' && viewdir != 'B') { dir = generateDirection(view) }
+      } else {
+        view.offsetToNearest('m').foreach (offset => dir = offset.signum.toString)
+        view.offsetToNearest('B').foreach (offset => dir = offset.signum.toString)
+        view.offsetToNearest('P').foreach (offset => dir = offset.signum.toString)
+        view.offsetToNearest('s').foreach (offset => dir = offset.signum.toString)
+        var viewdir = view.cellAtRelPos(XY(dir))
+        if (viewdir != '_' && viewdir != 'P' && viewdir != 'B' && viewdir != 'm' && viewdir != 's') { dir = generateDirection(view) }
+      }
+    } else {
+      // If time is nearly over, move everything to master
+      if (generation == 0) {
+        //view.offsetToNearest('S').foreach(offset => dir = offset.signum.toString)
+      } else {
+        // Enhance this: dir will be overwritten in main function
+        view.offsetToNearest('B').foreach (offset => dir = offset.signum.toString)
+        view.offsetToNearest('P').foreach (offset => dir = offset.signum.toString)
+        view.offsetToNearest('M').foreach(offset => dir = offset.signum.toString)
+      }
+
+      //Check direction
+      var viewdir = view.cellAtRelPos(XY(dir))
+
+      // Check if something bad is in the new way
+      // Masters and gen1 slaves likes only empty room, plants and good beasts
+      // Slaves gen2+ like enemy master and slaves too
+      if ( viewdir != '_' && viewdir != 'P' && viewdir != 'B' && viewdir != 'M') {
+        dir = generateDirection(view)
+      }
     }
+    dir
   }
 
-  def generateDirection(view: View) = {
+  def generateDirection(view: View): String = {
     import XY._
+    var dir = ""
 
-    var possibilities = List[XY](Right, RightUp, Up, UpLeft, Left, LeftDown, Down, DownRight)
-
+    val possibilities = List[XY](Right, RightUp, Up, UpLeft, Left, LeftDown, Down, DownRight)
     val goodPossibilities = possibilities.filter(p=> view.cellAtRelPos(p)=='_' || view.cellAtRelPos(p)=='P' || view.cellAtRelPos(p)=='B')
 
     if (goodPossibilities.isEmpty) {
-      val random = rnd.nextInt(possibilities.length)
+      var random = rnd.nextInt(possibilities.length)
       dir = possibilities(random).toString
     } else {
-      val random = rnd.nextInt(goodPossibilities.length)
+      var random = rnd.nextInt(goodPossibilities.length)
       dir = goodPossibilities(random).toString
     }
+
+    dir
   }
 }
 
